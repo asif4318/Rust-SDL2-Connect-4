a28f72f63eada92eec87ca25049019bc5d207bb6
Asif Islam <77113884+asif4318@users.noreply.github.com>
2024-11-19 21:48:30 -0500

    Ran autoformat

--- src/model/gamestate.rs
+++ src/model/gamestate.rs
@@ -2,26 +2,26 @@ use crate::model::tilestate::TileState;
 
 pub enum GameCondition {
     PLAYING,
-    ENDED
+    ENDED,
 }
 
 pub struct GameState {
     pub turn: TileState,
-    pub game_condition: GameCondition
+    pub game_condition: GameCondition,
 }
 
 impl GameState {
     pub fn new() -> Self {
         let turn = TileState::RED;
         let game_condition = GameCondition::PLAYING;
-        GameState{turn, game_condition}
+        GameState { turn, game_condition }
     }
 
     pub fn next_turn(&mut self) {
         match self.turn {
             TileState::EMPTY => {}
-            TileState::YELLOW => {self.turn = TileState::RED}
-            TileState::RED => {self.turn = TileState::YELLOW}
+            TileState::YELLOW => { self.turn = TileState::RED }
+            TileState::RED => { self.turn = TileState::YELLOW }
         }
     }
 
